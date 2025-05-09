d7876bcabbb3fa3ca0dc4f4286b9797e06cb6868
rohit <rohitthakur.eminence@gmail.com>
2025-05-09 19:25:58 +0530

    add history

--- app/MyRuntimeProvider.tsx
+++ app/MyRuntimeProvider.tsx
@@ -1,57 +1,72 @@
 "use client";
 
-import {
-
-  type ChatModelAdapter,
-} from "@assistant-ui/react";
+import { type ChatModelAdapter } from "@assistant-ui/react";
 import axios from "axios";
 
+function generateRandomEmail() {
+  const randomString = Math.random().toString(36).substring(2, 10); // 8 random characters
+  return `${randomString}@yopmail.com`;
+}
 
 
 export const MyModelAdapter: ChatModelAdapter = {
   async *run({ messages, abortSignal, context }) {
-    try{
-    console.log(messages, "messages", abortSignal, "abortSignal", context, "context")
-    const history = [];
-    const count = 5;
-    const start = messages.length > count ? messages.length - count : 0;
-    console.log(start)
-    const message: any = messages
-    for (let i = message.length - 1; i >= start; i--) {
-      console.log(message[i].role);
+    try {
+      console.log(
+        messages,
+        "messages",
+        abortSignal,
+        "abortSignal",
+        context,
+        "context"
+      );
+      const history = [];
+      const count = 5;
+      const start = messages.length > count ? messages.length - count : 0;
+      console.log(start);
+      const message: any = messages;
+      for (let i = message.length - 1; i >= start; i--) {
+        console.log(message[i].role);
 
-      const text = message[i].content[0].text;
-      if (message[i].role === "user") {
-        history.push(`user:${text}`);
-      } else {
-        history.push(`assistant:${text}`);
+        const text = message[i].content[0].text;
+        if (message[i].role === "user") {
+          history.push(`user:${text}`);
+        } else {
+          history.push(`assistant:${text}`);
+        }
       }
-    }
 
+      
+      const result = await axios.post(
+        "https://amus-devapi.musetax.com/tax_education/query",
+        {
+          query: message[messages.length - 1].content[0].text,
+          history: history,
+          email: generateRandomEmail()
+        }
+      );
+      console.log(result,'result?.data?.response?.response');
+      
+      // console.log(result)
+      const stream = result?.data?.response || "Something went wrong";
 
-    console.log(history)
-    const result = await axios.post('https://amus-devapi.musetax.com/tax_education/query', 
-      { "query": message[messages.length - 1].content[0].text})
-   // console.log(result)
-    const stream = result?.data?.response?.response || "Something went wrong"
-   
-   // const suggestions =[]
+      // const suggestions =[]
 
-   // let text = "";
-    // for await (const part of stream) {
-    //   text += part || "";
+      // let text = "";
+      // for await (const part of stream) {
+      //   text += part || "";
 
       yield {
-        content: [{ type: "text", text:stream }],
+        content: [{ type: "text", text: stream }],
         metadata: {
           // custom: {
           //   suggestions
           // }
-      }
+        },
       };
-   // }
-  }catch(error){
-    console.error("Error in TaxModelAdapter:", error);
+      // }
+    } catch (error) {
+      console.error("Error in TaxModelAdapter:", error);
 
       yield {
         content: [
@@ -61,7 +76,6 @@ export const MyModelAdapter: ChatModelAdapter = {
           },
         ],
       };
-
-  }
-}
-};  
+    }
+  },
+};
