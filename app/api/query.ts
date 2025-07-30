4ebe009044e9f5c9871c17925d9eca3c8da67698
XpertWebApp <appweb.live@gmail.com>
2025-07-30 15:09:58 +0530

    new changes

---
+++ app/api/query.ts
@@ -0,0 +1,18 @@
+ import { axiosInstance } from '@/utilities/axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req,'1req');
+  
+  if (req.method !== 'POST') return res.status(405).json({ message: 'Method Not Allowed' });
+
+  const { query } = req.body;
+
+  try {
+    const response = await axiosInstance.post(`/query`, { query });
+    res.status(200).json(response.data);
+  } catch (error: any) {
+    res.status(error.response?.status || 500).json(error.response?.data || { message: error.message });
+  }
+}
